810f44c2be30795b65e25a1836ba3df3a5106c1b
Venc03 <vegi.dani1@gmail.com>
2023-10-02 14:04:45 +0200

    infinit loop a melyikben, tovabbi mukodest nem tudtam tesztelni

--- src/ora1002/Ora1002.java
+++ src/ora1002/Ora1002.java
@@ -12,9 +12,9 @@ public class Ora1002 {
 
         feltolt();
         for (int i = 0; i < 3; i++) {
-            melyik();
+            int oszlop = melyik();
+            kever(oszlop);
             kirak();
-            kever();
         }
         ezVolt();
 
@@ -35,45 +35,50 @@ public class Ora1002 {
     }
     
     public static void kirak() {
-        for (int i = 0; i < pakli.length; i++) {
-            System.out.printf(pakli[i] + "d8%");
+        for (int i = 1; i < pakli.length; i++) {
+            System.out.printf(pakli[i] + " ");
         }
     }
 
     public static int melyik() {
-        int oszlop = 0;
-        while (oszlop <= 2 && oszlop >= 0) {  
+        int oszlop = src.nextInt();
+        do {            
             System.out.println("Kérem adja meg az oszlopot");
             oszlop = src.nextInt();
-        }
+        } while (oszlop < 3 || oszlop > 1);
+        
         return oszlop;
     }
 
     public static void kever(int oszlop) {
         String[] ujPakli = new String[22];
-        for (int i = 1; i < 10; i++) {
-            
-        }
         switch (oszlop) {
             case 1:
-                ujPakli[i] = (20 - (i - 1) * 3);
-                ujPakli[i+7] = (19 - (i - 1) * 3);
-                ujPakli[i+14] = (21 - (i - 1) * 3);
+                for (int i = 1; i < 7; i++) {
+                    ujPakli[i] = pakli[20 - (i - 1) * 3];
+                    ujPakli[i+7] = pakli[19 - (i - 1) * 3];
+                    ujPakli[i+14] = pakli[21 - (i - 1) * 3];
+                }
                 
             case 2:
-                ujPakli[i] = (19 - (i - 1) * 3);
-                ujPakli[i+7] = (20 - (i - 1) * 3);
-                ujPakli[i+14] = (21 - (i - 1) * 3);
+                for (int i = 0; i < 7; i++) {
+                    ujPakli[i] = pakli[19 - (i - 1) * 3];
+                    ujPakli[i+7] = pakli[20 - (i - 1) * 3];
+                    ujPakli[i+14] = pakli[21 - (i - 1) * 3];
+                }
                 
             case 3:
-                ujPakli[i] = (19 - (i - 1) * 3);
-                ujPakli[i+7] = (21 - (i - 1) * 3);
-                ujPakli[i+14] = (20 - (i - 1) * 3);
+                for (int i = 0; i < 7; i++) {
+                    ujPakli[i] = pakli[19 - (i - 1) * 3];
+                    ujPakli[i+7] = pakli[21 - (i - 1) * 3];
+                    ujPakli[i+14] = pakli[20 - (i - 1) * 3];
+                }
         }
     }
 
     public static void ezVolt() {
-
+        
+        System.out.println("Ez volt a lapja: " + pakli[11]);
     }
 
     
